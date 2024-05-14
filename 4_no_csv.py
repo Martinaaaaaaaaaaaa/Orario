771e14c99b30d9aadbd9583a63eb0cce6693d05e
ThisAsh <thenameisashlynn@gmail.com>
2024-05-14 20:41:34 +0200

    4_fine

--- 4_no_csv.py
+++ 4_no_csv.py
@@ -2,7 +2,7 @@
 
 #funzione
 def docenti_lezione(ora,giorno):
-    giorni = ('Lu','Ma','Me','Gi','Ve')
+    giorni = ('lun','mar','mer','gio','ven')
     file = open('OrarioTabellaGlobale.csv','r') #apertura file in modalità lettura
     campi = next(file)
     ore = next(file)
@@ -14,25 +14,27 @@ def docenti_lezione(ora,giorno):
     cell = numero_iniziale + ora #posizione dell'elemento da scansionare
     for row in file: #scansione
         row = row.split(',')
-        
-        if  '  ' in row[cell]: #se la cella è vuota il docente non ha lezione
+        try:
+            if  '  ' in row[cell]: #se la cella è vuota il docente non ha lezione
+                continue
+            else:
+                nome_docente = row.pop(0)
+                elenco.append(nome_docente)
+        except IndexError: #quando il valore della variabile 'cell' arriva ad essere maggiore dell'indice della lista
             continue
-        else:
-            nome_docente = row.pop(0)
-            elenco.append(nome_docente)
     file.close() #chiusura file
     return elenco
 
 while True: #loop infinito
     #input utente
     in_ora = int(input('Inserire ora scelta: '))
-    in_giorno = input('Inserire prime 2 lettere del giorno scelto: ')
+    in_giorno = input('Inserire prime 3 lettere del giorno scelto in minuscolo: ')
     
     #chiamata funzione
     docenti = docenti_lezione(in_ora, in_giorno)
 
     #stampa risultato
-    print(f'I docenti che hanno lezione alla {in_ora} ora il {in_giorno} sono: \n')
+    print(f'I docenti che hanno lezione alla {in_ora} ora il {in_giorno} sono:')
     for elem in docenti:
         print(elem)
 

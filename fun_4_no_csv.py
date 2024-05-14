665521005b8efe96662e0e816f2fcc3c3d2ee03a
ThisAsh <thenameisashlynn@gmail.com>
2024-05-14 21:30:31 +0200

    fine

--- fun_4_no_csv.py
+++ fun_4_no_csv.py
@@ -25,16 +25,3 @@ def docenti_lezione(ora,giorno):
     file.close() #chiusura file
     return elenco
 
-while True: #loop infinito
-    #input utente
-    in_ora = int(input('Inserire ora scelta: '))
-    in_giorno = input('Inserire le prime 3 lettere del giorno scelto: ').lower()
-    
-    #chiamata funzione
-    docenti = docenti_lezione(in_ora, in_giorno)
-
-    #stampa risultato
-    print(f'I docenti che hanno lezione alla {in_ora} ora il {in_giorno} sono:')
-    for elem in docenti:
-        print(elem)
-
